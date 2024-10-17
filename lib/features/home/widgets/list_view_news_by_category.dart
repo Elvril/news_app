708f3bdd73a201a3e60f5e45d2da6a55502e0ca1
khaled slimane tich tich <stt.khaled@gmail.com>
2024-10-17 20:19:17 +0100

    added refrech indicator

--- lib/features/home/widgets/list_view_news_by_category.dart
+++ lib/features/home/widgets/list_view_news_by_category.dart
@@ -7,7 +7,7 @@ import 'package:news_app/widgets/articles_list_view.dart';
 import 'package:news_app/models/top_headlines/top_headlines.dart';
 import 'package:news_app/widgets/error_dialog.dart';
 
-class ListViewNewsByCategory extends ConsumerWidget {
+class ListViewNewsByCategory extends ConsumerStatefulWidget {
   final String category;
   const ListViewNewsByCategory({
     super.key,
@@ -15,48 +15,64 @@ class ListViewNewsByCategory extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ListViewNewsByCategory> createState() =>
+      _ListViewNewsByCategoryState();
+}
+
+class _ListViewNewsByCategoryState
+    extends ConsumerState<ListViewNewsByCategory> {
+  @override
+  Widget build(BuildContext context) {
     bool isDialogShowing = false;
-    final AsyncValue<TopHeadlines> responseAsync =
-        ref.watch(fetchNewsByCategoryProvider(1, category));
+    AsyncValue<TopHeadlines> responseAsync =
+        ref.watch(fetchNewsByCategoryProvider(1, widget.category));
     final totalResult = responseAsync.valueOrNull?.totalResults;
-    return ListView.builder(
-      itemCount: totalResult,
-      itemBuilder: (context, index) {
-        final page = index ~/ pageSize + 1;
-        final indexInPage = index % pageSize;
-        final AsyncValue<TopHeadlines> responseAsync =
-            ref.watch(fetchNewsByCategoryProvider(page, category));
-        return responseAsync.when(
-          error: (err, stack) {
-            if (!isDialogShowing) {
-              isDialogShowing = true;
-              Future.delayed(
-                Duration.zero,
-                () {
-                  errorDialog(context, (err as CustomError).errorMessage);
-                },
-              );
-            }
-            return null;
-          },
-          loading: () => Container(
-            width: 364,
-            height: 183,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-            ),
-            child: const Center(child: CircularProgressIndicator()),
-          ),
-          data: (data) {
-            if (indexInPage >= data.articles.length) {
-              return null;
-            }
-            final article = data.articles[indexInPage];
-            return ArticlesListView(article: article);
-          },
-        );
+    return RefreshIndicator(
+      displacement: 5,
+      color: Colors.white,
+      backgroundColor: const Color(0xff76777B),
+      onRefresh: () async {
+        return await ref
+            .refresh(fetchNewsByCategoryProvider(1, widget.category));
       },
+      child: ListView.builder(
+        itemCount: totalResult,
+        itemBuilder: (context, index) {
+          final page = index ~/ pageSize + 1;
+          final indexInPage = index % pageSize;
+          final AsyncValue<TopHeadlines> responseAsync =
+              ref.watch(fetchNewsByCategoryProvider(page, widget.category));
+          return responseAsync.when(
+            error: (err, stack) {
+              if (!isDialogShowing) {
+                isDialogShowing = true;
+                Future.delayed(
+                  Duration.zero,
+                  () {
+                    errorDialog(context, (err as CustomError).errorMessage);
+                  },
+                );
+              }
+              return null;
+            },
+            loading: () => Container(
+              width: 364,
+              height: 183,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: const Center(child: CircularProgressIndicator()),
+            ),
+            data: (data) {
+              if (indexInPage >= data.articles.length) {
+                return null;
+              }
+              final article = data.articles[indexInPage];
+              return ArticlesListView(article: article);
+            },
+          );
+        },
+      ),
     );
   }
 }
